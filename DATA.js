e13e327d1d82d5d40e57570309b8b161361d8554
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 13:21:02 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "cd3d4e00-1f58-410c-9929-40a0152679f2",
+    "home_feed": [
+        {
+            "id": "ff1f3f13-bba5-467a-9fa2-6eba11dedfc8",
+            "artist_name": "religious",
+            "home_carousel": [
+                {
+                    "id": "65c930c6-d813-4438-b2e6-6fa05526509e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                },
+                {
+                    "id": "60c0e35d-4e85-4fec-9f55-b33082341aa8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/283.png"
+                },
+                {
+                    "id": "9dbc13d7-8042-4265-94ff-a002f9189f33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "46d6e068-adba-4e5b-aa71-e86f25a0c97c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "c8b44034-7442-49bd-adbc-73f2264fb3b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                }
+            ],
+            "art_caption": "quickly",
+            "art_description": "Like wide after various most wait. Economy large back him now tonight save quite."
+        },
+        {
+            "id": "39b7bf6f-42fc-49fd-aada-704abe16155d",
+            "artist_name": "school",
+            "home_carousel": [
+                {
+                    "id": "a50ff3b0-a051-4f16-92bd-1b068298ad67",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+                },
+                {
+                    "id": "be59c89a-7aeb-4743-b709-6709425f2088",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "34439fac-b1a4-4d21-8a49-a75c01707f7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png"
+                },
+                {
+                    "id": "39751552-e0d4-4253-a994-3d8f41b1e2a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "09da731b-1ee8-43e0-a301-62c2b0324c92",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                }
+            ],
+            "art_caption": "religious",
+            "art_description": "Herself practice according cultural. Final writer tax huge decade must."
+        },
+        {
+            "id": "f315bcb9-7093-42db-b051-3c14fba6caa0",
+            "artist_name": "effect",
+            "home_carousel": [
+                {
+                    "id": "0afb5aef-4233-493b-91e2-0f1c0d31889e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "8f8209aa-68b0-4d45-a52e-6f22b98624eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                },
+                {
+                    "id": "5ec4e3e7-e060-41db-ba34-1ede37b214af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "0a33dd7d-4265-458a-bbb0-e9fc3cdc968a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/100.png"
+                },
+                {
+                    "id": "5c6ec27d-5d67-4669-b60b-0810f3123493",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                }
+            ],
+            "art_caption": "serious",
+            "art_description": "Drop his investment provide successful."
+        },
+        {
+            "id": "9bbad654-7b4d-46b5-a120-3a1eb041c382",
+            "artist_name": "play",
+            "home_carousel": [
+                {
+                    "id": "4bb0ed3b-f368-4011-9e35-89dcdc8e4937",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "edf9b59e-d71e-4ecc-b790-2da8441525fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "696740f8-7b00-4033-a21b-4551befcbd49",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png"
+                },
+                {
+                    "id": "4251761b-8b52-4cee-b753-6b7b30938546",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "06757686-dd5b-4d0e-85b9-abe5e6489600",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                }
+            ],
+            "art_caption": "herself",
+            "art_description": "Fish best beautiful scene. Small technology manager Mr it station."
+        },
+        {
+            "id": "e1fac15f-1a29-489a-bba1-b7132406c0b5",
+            "artist_name": "stuff",
+            "home_carousel": [
+                {
+                    "id": "77db4fd7-3e5b-4f81-847f-90243da09f8c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "ea4aa8d7-a84b-4bd8-aab7-8a65c84e532d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "71a817fd-dccf-4325-b00c-a0efb919a3b1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png"
+                },
+                {
+                    "id": "b3d50f35-2010-41d5-91f6-4a7e65ccf321",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "bd20f07c-0aa1-43ed-a4b4-90268410ea6f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                }
+            ],
+            "art_caption": "recently",
+            "art_description": "Administration billion wonder enough your after chance think. Rise service fire day partner forward."
+        }
+    ]
+}
